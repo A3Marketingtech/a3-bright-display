e232773cb936e62eeb663b465afd9be8a5924f9b
A3Marketingtech <curso@a3marketing.tech>
2026-04-06 15:41:28 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+ base: '/a3-bright-display/',
   server: {
     host: "::",
     port: 8080,
